2cc21376c48c2768a381c40f4b7a8e994f6ce86e
jbrownxf <joshbrown2195@gmail.com>
2018-05-21 18:19:51 -0400

    added ip_input

---
+++ lab_input/input_ip.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+#author- josh brown
+# to collect user ip information and display it back to them
+
+#user's input for thier ip address
+user_input = input('Please enter an IPv4 IP address:')
+
+##prints the user's input to verify
+print("You told me teh IPv4 address is:" + user_input)
