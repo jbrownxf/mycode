6cc0dbb5d29b8f83608817389674d6e54f4462c4
jbrownxf <joshbrown2195@gmail.com>
2018-05-22 19:01:26 -0400

    if loop lab

---
+++ if-test/if-hostname.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Testing with if lab
+
+hostname = input("What is your hostname? ") #promtps user for hostname
+
+#if the user input matches any of these it is true
+if hostname.lower() == 'mtg': 
+    #If statement is true, this prints
+    print('hostname matches expected config') 
+
+print('Exiting the script')
---
+++ mix-list3/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# animal list
+
+alist = ['cow', 'ant', 'cat', 'dog', 'hen']
+
+for j in alist:
+    print(j, end= " ")
+print()
+
