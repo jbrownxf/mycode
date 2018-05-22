f9256add455d5c9448cca60d8b9208b139df608a
jbrownxf <joshbrown2195@gmail.com>
2018-05-22 11:48:24 -0400

    ip mac address return made

---
+++ day2/ip_mac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Script to accept user inputs for ip and mac addresses and displays them.
+
+#request for user addresses
+print('What is your ip address?')
+IP= input()
+
+print('what is your Mac Address?')
+MAC= input()
+
+
+#print return for the addresses
+print('Your ip address is:' + str(IP) + '   ' + 'Your mac address is:' + str(MAC))
+
+
+	
