d7575be9c9e666757a2272da0e71105bab8c89b1
jbrownxf <joshbrown2195@gmail.com>
2018-05-31 16:17:14 -0400

    finished_time_datetime_lab

--- tick-tock/custom.py
+++ tick-tock/custom.py
@@ -10,10 +10,18 @@ ctsecs = int(time.time()) #epoch time in secs
 bdtsecs = time.mktime(bday.timetuple()) # import the user date into seconds
 #print(bdtsecs)
 #tdelta = #epoch time in secs minus mybday in secs
-tdelta = ctsecs - bdtsecs #turn human time into second
-#print(tdelta)
+tdelta = ctsecs - bdtsecs
+print(tdelta)
 
 #convert tdelta from secs to days
+tdeltadays = int(tdelta) / int((60*60*24))
+print(str(round(tdeltadays)) + ' days since my birthday')
 
-tdeltasec = int(tdelta) / int((60*60*24))
-print(str(round(tdeltasec)) + ' days since my birthday')
+#Shows the curretn time plus 500 seconds
+f5sec = datetime.datetime.now() + datetime.timedelta(seconds=500)
+print(f5sec)
+print(time.mktime(f5sec.timetuple()))
+#Shows the curretn time plus 500 days
+f5day = datetime.datetime.now() + datetime.timedelta(days=500)
+print(f5day)
+print(time.mktime(f5day.timetuple()))
